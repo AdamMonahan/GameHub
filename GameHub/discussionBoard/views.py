52425f5c443f9b28edadadca939c536a396228da
Adam Monahan <amonahan0995@gmail.com>
2020-08-12 17:30:10 -0400

    Building Discussion Board

--- GameHub/discussionBoard/views.py
+++ GameHub/discussionBoard/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from login.models import User
 
 # Create your views here.
+def discuss(request):
+    if 'user_id' not in request.session:
+        return redirect('/') 
+    context = {
+        'user': User.objects.get(id = request.session['user_id'])
+    }
+    return render(request, 'discussions/index.html', context)
