a0d5672bbe69a632b0da926ed336e95f7a51ce71
Adam Monahan <amonahan0995@gmail.com>
2020-08-17 19:59:11 -0400

    discussion board basics

--- GameHub/GameHub/urls.py
+++ GameHub/GameHub/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('login.urls')),
-    path('register/', include('register.urls')),
     path('discuss/', include('discussionBoard.urls')),
+    path('register/', include('register.urls')),
     path('admin/', admin.site.urls),
 ]
--- GameHub/login/urls.py
+++ GameHub/login/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
-    path('create', views.create),
+    #path('create', views.create),
     path('login', views.login),
     path('logout', views.logout),
     path('success', views.success),
